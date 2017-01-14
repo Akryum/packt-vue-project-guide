dfa60b01fcaab1eba3606f01f5345dc7dabad02c
Guillaume Chau <guillaume.b.chau@gmail.com>
2017-01-14 22:05:14 +0100

    Download kit fixes

---
+++ chapter3-download/state.js
@@ -0,0 +1,11 @@
+var maxHealth = 10
+var maxFood = 10
+var handSize = 5
+var cardUid = 0
+var currentPlayingCard = null
+
+var state = {
+  // World
+  worldRatio: getWorldRatio(),
+  // TODO Other things
+}
