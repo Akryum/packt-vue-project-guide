c530860865389d933ce00dcdc435e43029ac4759
Guillaume Chau <guillaume.b.chau@gmail.com>
2017-01-14 22:13:26 +0100

    More comments

--- chapter3/state.js
+++ chapter3/state.js
@@ -1,9 +1,11 @@
+// Some usefull variables
 var maxHealth = 10
 var maxFood = 10
 var handSize = 5
 var cardUid = 0
 var currentPlayingCard = null
 
+// The consolidated state of our app
 var state = {
   // UI
   activeOverlay: null,
