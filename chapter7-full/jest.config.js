6c3f812aff5099ca9f5db19e957dc350ee58cef7
Guillaume Chau <alphadelta.fg@gmail.com>
2017-11-24 11:51:00 +0100

    Chapter 7: Update deps

--- chapter7-full/jest.config.js
+++ chapter7-full/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   transform: {
     '.+\\.jsx?$': '<rootDir>/node_modules/babel-jest',
-    '.+\\.vue$': '<rootDir>/node_modules/jest-vue',
+    '.+\\.vue$': '<rootDir>/node_modules/vue-jest',
   },
   snapshotSerializers: [
     '<rootDir>/node_modules/jest-serializer-vue',
